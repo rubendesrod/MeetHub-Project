a13010739900094ad2e6f0dc8e97d0d5b10a0adb
rubenPisoValladolid <rubendr@alumnos.iesgalileo.es>
2024-03-14 12:14:02 +0100

    Incorporación del Spring Project

---
+++ project/src/test/java/com/meethub/project/MeetHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meethub.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
