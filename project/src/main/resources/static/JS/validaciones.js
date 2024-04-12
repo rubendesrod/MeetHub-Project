265e373699519451bc38537f514e1b9e816af461
rubenPisoValladolid <rubendr@alumnos.iesgalileo.es>
2024-04-12 20:51:34 +0200

    Login - V.FINAL

---
+++ project/src/main/resources/static/JS/validaciones.js
@@ -0,0 +1,12 @@
+// En este archivo irán todas la validaciones necesarias para los formularios
+
+// Login
+const emailL = document.getElementById("emailL");
+const passL = document.getElementById("passwordL");
+
+// funciones
+function validarLogin(event){
+    alert("Funciona");
+     // Con la siguiente línea el formulario no es enviará
+     event.preventDefault();
+}
