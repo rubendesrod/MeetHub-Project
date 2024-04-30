cf3ffef65fd95e69bb0cbcd3f9ba45aa2fcda1ba
rubenPisoValladolid <rubendr@alumnos.iesgalileo.es>
2024-04-30 19:09:40 +0200

    CalendarioV.FINAL

--- project/src/main/resources/static/JS/calendario.js
+++ project/src/main/resources/static/JS/calendario.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: "dayGridMonth",
     locale: "es",
+    firstDay: 1, // para que el calendario empiece en lunes
     headerToolbar: {
       left: "prev,next",
       center: "title",
@@ -18,17 +19,18 @@ document.addEventListener("DOMContentLoaded", function () {
       {
         title: "Evento 1",
         start: "2024-04-01",
-        color: "#ff9f89",
         description: "hola",
-      }, // color de fondo
+        color: "#f4c22b"
+      },
       {
         title: "Evento 2",
         start: "2024-04-05",
         end: "2024-04-07",
         color: "#f4c22b",
+        description: "ruben no sabe que mas poner"
       },
     ],
-
+    // Evento para cuando el ratón pase por enicma de uno de los eventos del calendario
     eventMouseEnter: function (info) {
       tippy(info.el, {
         content: info.event.extendedProps.description,
@@ -37,6 +39,15 @@ document.addEventListener("DOMContentLoaded", function () {
         theme: "light-border", // Usa un tema predefinido o crea uno personalizado
       });
     },
+    // Evento para cuando se haga clic en una día
+    dateClick: function(info) {
+      // Llenar el modal con información o hacer cualquier preparación necesaria
+      document.getElementById('modalLabel').innerHTML = 'Agregar Evento [' + info.dateStr +']';
+
+      // Usar Bootstrap para abrir el modal
+      var modal = new bootstrap.Modal(document.getElementById('dayClickModal'));
+      modal.show();
+    }
   });
   calendar.render();
 });
